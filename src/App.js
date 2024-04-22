897ad75de9cc6b2568f8c527bf449ce311b102d0
Rachel-Taskale <racheltaskale@gmail.com>
2024-04-22 14:04:54 -0400

    added svg designs and links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import * as React from "react";
 import { useState, useEffect, useRef } from "react";
-import AnchorLink from "react-anchor-link-smooth-scroll";
-import { HashLink } from "react-router-hash-link";
-import { NavLink } from "react-router-dom";
-// import ScrollHighlightNabbar from "./navbar/navbar";
 import "./input.css";
 import {
-  MdLocalPhone,
-  MdEmail,
-  MdOutlineAccountTree,
-  MdAccountTree,
-  MdFileCopy,
-  MdOutlineSmartphone,
-  MdOutlineWoman2,
   MdOpenInNew,
-} from "react-icons/md";
-import { IoLogoGithub } from "react-icons/io";
+  MdArrowRightAlt
 
-import { FaLinkedinIn } from "react-icons/fa";
-import { get } from "jquery";
+} from "react-icons/md";
 
 export default function App() {
   const [mobileView, setMobileView] = useState(
@@ -39,19 +26,19 @@ export default function App() {
       img: "https://imgur.com/jkpRzD3.jpg",
     },
     {
-      title: "Fish Illustration",
+      title: "Fishy Illustration",
       subtitle: "Gouache + Colored Pencil",
       link: "https://www.instagram.com/p/CYHfSGgF5SO/",
       img: "https://imgur.com/Q4T5eU2.jpg",
     },
     {
-      title: "GenZ Zoom Feature",
+      title: "Funny UI - GenZ Zoom Feature",
       subtitle: "Figma",
       link: "https://www.linkedin.com/posts/racheltaskale_10xgrowth-impact-startup-activity-7063561214085279744-x_m6?utm_source=share&utm_medium=member_desktop",
       img: "https://i.imgur.com/U1HrxwC.png",
     },
     {
-      title: "Spotify X BetterHelp",
+      title: "Funny UI - Spotify X BetterHelp",
       subtitle: "Figma",
       link: "https://www.linkedin.com/posts/racheltaskale_hypemusiconly-mentalhealthawareness-partnership-activity-7069488582373048320-TLvC?utm_source=share&utm_medium=member_desktop",
       img: "https://imgur.com/bKWsZ7H.jpg",
@@ -72,32 +59,36 @@ export default function App() {
 
   const softwareStuff = [
     {
-      title:"Traffic Replay",
-      description:"Optimized reconciliation reporting with AWS Lambda and enhanced concurrency. Scaled our capacity to examine mainframe-cloud disparities in trade enrichment from 48,000 to 10 million messages while maintaing report generation under an hour",
-      link_tag:<div className="font-bold h-fit w-fit px-4 py-2 subtitle rounded-sm border-2 border-background">AWS + Kafka</div>,
-      version:"light"
+      title: "Traffic Replay",
+      description: "Optimized reconciliation reporting with AWS Lambda and enhanced concurrency. Scaled our capacity to examine mainframe-cloud disparities in trade enrichment from 48,000 to 10 million messages while maintaing report generation under an hour.",
+      link_tag: <div className="font-bold h-fit w-fit px-4 py-2 subtitle rounded-sm border-2 border-background">AWS + Kafka</div>,
+      img: "traffic-replay-pattern m-10",
+      mode: "bg-t-secondary text-background"
     },
     {
-      title:"ConnectJew",
-      description:"Developed web application for nonprofit to crowdsource and display all Jewish Community events for the DC, Maryland, and Virginia area.",
-      link_tag:<a className="font-bold h-fit w-fit px-4 py-2 subtitle rounded-sm border-2 
+      title: "ConnectJew",
+      description: "Developed web application for nonprofit to crowdsource and display all Jewish Community events for the DC, Maryland, and Virginia area.",
+      link_tag: <a className="font-bold h-fit w-fit px-4 py-2 subtitle rounded-sm border-2 
       border-t-secondary hover:bg-t-secondary hover:text-background" href="https://connectjew.org" target="_blank">Website</a>,
-      version:"dark"
+      img: "connect-jew-pattern",
+      mode: "text-t-secondary"
     },
     {
-      title:"Women Investors",
-      description:"Business case for Wealth Management to support women investors through competitive analysis, gap analysis, internal stakeholder & client interviews, and product development.",
-      link_tag:<div className="h-fit w-fit px-4 py-2 subtitle rounded-sm font-bold border-2 border-background">Figma + Product Design</div>,
-      version:"light"
+      title: "Women Investors",
+      description: "Business case for Wealth Management to support women investors through competitive analysis, gap analysis, internal stakeholder & client interviews, and product development.",
+      link_tag: <div className="h-fit w-fit px-4 py-2 subtitle rounded-sm font-bold border-2 border-background">Figma + Product Design</div>,
+      img: "women-investors-pattern",
+      mode: "bg-t-secondary text-background"
     },
     {
-      title:"Tree Identification",
-      description:"Brooklyn native tree image classifier using custom pytorch functions in place of BatchNorm2D, Conv2D, MaxPooling2D, and ReLU",
-      link_tag:<a className="font-bold h-fit w-fit px-4 py-2 subtitle rounded-sm border-2 
+      title: "Tree Identification",
+      description: "Brooklyn native tree image classifier using custom pytorch functions in place of BatchNorm2D, Conv2D, MaxPooling2D, and ReLU.",
+      link_tag: <a className="font-bold h-fit w-fit px-4 py-2 subtitle rounded-sm border-2 
       border-t-secondary hover:bg-t-secondary hover:text-background"
-      href="https://github.com/rachel-taskale/TreeClassifier"
-      target="_blank">Github</a>,
-      version:"dark"
+        href="https://github.com/rachel-taskale/TreeClassifier"
+        target="_blank">Github</a>,
+      img: "tree-pattern",
+      mode: "text-t-secondary"
     },
   ]
 
@@ -115,39 +106,47 @@ export default function App() {
   }, []);
 
   return (
-    <div className="App bg-background pt-10 pb-40 px-24 ">
+    <div className="App bg-background pt-10 pb-40 px-10 lg:px-24 2xl:px-40 ">
       <div className="w-full flex place-content-end">
-        <button className="col-span-1 justify-self-end font-bold border-2 border-t-secondary 
-                            text-t-secondary h-fit w-fit px-4 py-2 subtitle rounded-sm">
+        <a className="col-span-1 justify-self-end text-xl 
+                            text-t-secondary h-fit w-fit px-4 py-2 header rounded-sm"
+          href="https://docs.google.com/document/d/1lrs6NtBp28DJeWjRg3X2ZvDaU9CjjJ3CYRKNe66S8sI/edit?usp=sharing"
+          target="_blank"
+        >
           RESUME
-        </button>
+        </a>
       </div>
-      <div className="space-y-56 pt-32 2xl:pt-80">
-        <div className="grid grid-cols-4">
-          <div className="header text-t-primary text-4xl 2xl:text-5xl col-span-3 leading-relaxed 4xl:leading-loose">
-            Rachel Taskale is an engineer & designer based in Brooklyn, NY.{" "}
-            <span className="text-t-secondary">
-              Her goal is to build a more equitable and creative future. In her
-              free time, you can find her wandering through New York City and
-              drawing.
-            </span>
+      <div className="space-y-80 lg:space-y-56 2xl:space-y-80 ">
+        <div className="w-full mb-24">
+          <div className="w-full mb-20 flex place-content-center">
+            <img className="" src="/profileDrawing.svg" alt="Profile Drawing" />
+          </div>
+          <div className="grid grid-cols-4">
+            <div className="header text-t-primary text-4xl 2xl:text-5xl col-span-3 leading-relaxed 4xl:leading-loose">
+              Rachel Taskale is an engineer & designer based in Brooklyn, NY.{" "}
+              <span className="text-t-secondary">
+                Her goal is to build a more equitable and creative future. In her
+                free time, you can find her wandering through New York City and
+                drawing.
+              </span>
+            </div>
           </div>
         </div>
 
-        <div className="flex flex-wrap gap-8">
-          <div className="header w-56 h-64 text-t-primary text-4xl flex items-center leading-relaxed">
-            <div>Creative Projects</div>
+        <div className="flex flex-wrap gap-4 lg:gap-10 2xl:gap-20">
+          <div className="header w-72 h-80 text-t-primary text-4xl flex items-center leading-relaxed">
+            <div >Creative Projects</div>
           </div>
           {designStuff.map((item) => (
             <div className="image-container">
               <img
-                className="w-56 h-64 rounded-md object-cover"
+                className="w-72 h-80 rounded-md object-cover"
                 src={item.img}
               />
               <div className="overlay text-t-primary rounded-md px-4 place-content-center h-full w-full">
-              {item.link!=="" ? <a className="flex place-content-end text-2xl" href={item.link} target="_blank">
+                {item.link !== "" ? <a className="flex place-content-end text-2xl" href={item.link} target="_blank">
                   <MdOpenInNew className="absolute right-2 top-2" />
-                </a>: null
+                </a> : null
                 }
                 <div className="space-y-10">
                   <p className="header text-4xl">{item.title}</p>
@@ -156,50 +155,37 @@ export default function App() {
               </div>
             </div>
           ))}
+          <div className="w-full flex place-content-end text-t-secondary">
+            <a className="font-bold h-fit w-fit px-4 py-2 subtitle rounded-sm 
+      border-t-secondary hover:bg-t-secondary hover:text-background flex" href="https://instagram.com/art_by_task" target="_blank">Art Instagram <MdArrowRightAlt className="text-2xl" /> </a>
+          </div>
         </div>
+
         <div>
           <div className="flex justify-center py-8">
-            <div className="text-t-primary header text-4xl">
+            <div className="text-t-primary mb-10 header text-4xl">
               Software Projects
             </div>
           </div>
           <div className="space-y-40">
-            {softwareStuff.map((item)=>(
-              item.version === 'light' ? 
-              <div className="bg-t-secondary inline-block p-20 lg:flex lg:grid lg:grid-cols-3 gap-8">
-              <div className="w-full flex justify-center pb-20 lg:pb-0">
-              <div className="w-56 h-64 rounded-md lg:col-span-1 bg-background"></div>
-              </div>
-              <div className="lg:col-span-2 space-y-8 px-20 w-full">
-                <div className="text-background header leading-relaxed text-4xl">
-                  {item.title}
+            {softwareStuff.map((item) => (
+              <div className={`${item.mode} inline-block p-20 lg:flex lg:grid lg:grid-cols-3 gap-8 rounded-lg`}>
+                <div className="w-full flex justify-center pb-20 lg:pb-0">
+                  <div className={`${item.img} md:w-56 md:h-64 lg:w-72 lg:h-80 rounded-md lg:col-span-1 bg-background`} />
                 </div>
-                <div className="subtitle text-background leading-relaxed">
-                  {item.description}
-                </div>
-                <div className="w-full flex place-content-end">
-                  {item.link_tag}
-                </div>
-              </div>
-            </div>
-            :
-            <div className="bg-background text-t-secondary inline-block p-20 lg:flex lg:grid lg:grid-cols-3 gap-8">
-            <div className="w-full flex justify-center pb-20 lg:pb-0">
-              <div className="w-56 h-64 rounded-md lg:col-span-1 bg-t-secondary"></div>
-              </div>
-              <div className="lg:col-span-2 space-y-8 px-20">
-                <div className=" header leading-relaxed text-4xl">
-                  {item.title}
-                </div>
-                <div className="subtitle leading-relaxed">
-                  {item.description}
-                </div>
-                <div className="w-full flex place-content-end">
-                  {item.link_tag}
+                <div className="lg:col-span-2 space-y-8 px-20 w-full">
+                  <div className="header leading-relaxed text-4xl">
+                    {item.title}
+                  </div>
+                  <div className="subtitle text-lg leading-relaxed">
+                    {item.description}
+                  </div>
+                  <div className="w-full flex text-lg place-content-end">
+                    {item.link_tag}
+                  </div>
                 </div>
               </div>
-            </div>
-              ))}
+            ))}
           </div>
         </div>
       </div>
